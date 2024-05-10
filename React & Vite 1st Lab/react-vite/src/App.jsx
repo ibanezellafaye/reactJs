f32b2494fb1d773ac316d4463ea43887e330b796
ibanezellafaye <160290890+ibanezellafaye@users.noreply.github.com>
2024-05-10 16:18:38 +0800

    react-js-installation

---
+++ React & Vite 1st Lab/react-vite/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Vite + React + Tailwindcss</h1>
+      <h3>Hello, everyone! This is Ella Faye Ibañez from BSIT-2R2</h3>
+    </>
+  )
+}
+
+export default App
